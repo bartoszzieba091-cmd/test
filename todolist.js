a1b41332adf157d6b709334c1dd94c9b25915c5a
bartoszzieba091-cmd <bartoszzieba091@gmail.com>
2026-05-24 20:47:50 +0200

    version 1

--- todolist.js
+++ todolist.js
@@ -1,3 +1,4 @@
+
 const input = document.querySelector('input');
 const myList = document.querySelector('ul');
 let completedCounter = 0;
@@ -5,7 +6,9 @@ let completedCounter = 0;
 function addToStorage(v){
     localStorage.setItem(v, '1');
 }
+
 function addElementToList(v, operation){
+
     const newObject = document.createElement('li');
     newObject.textContent = v;
     newObject.value = v;
@@ -15,21 +18,20 @@ function addElementToList(v, operation){
     const newButton = document.createElement('button');
 
     if (operation == 1){
-    newButton.textContent = 'Done';
-    newButton.classList.add('done'); 
+        newButton.textContent = 'Done';
+        newButton.classList.add('done'); 
     }else{
-    newButton.textContent = 'not!';
-    newButton.classList.add('notd');
-    newObject.style.textDecoration = 'line-through'
+        newButton.textContent = 'not!';
+        newButton.classList.add('notd');
+        newObject.style.textDecoration = 'line-through'
     }
+
     const newButtonDel = document.createElement('button');
     newButtonDel.textContent = 'Delete';
     newButtonDel.classList.add('delete');
 
     newObject.appendChild(newButton);
     newObject.appendChild(newButtonDel);
-    
-    console.log(localStorage.length);
 }
 
 input.addEventListener('keydown', function(e) {
@@ -41,46 +43,49 @@ input.addEventListener('keydown', function(e) {
 });
 
 document.body.addEventListener('click', function(b){
+
     if (b.target.id === 'add') {
+
         addElementToList(input.value, 1);
         addToStorage(input.value);
         input.value = '';
     } 
     else if (b.target.id === 'allDone') {
+
         completedCounter += +myList.children.length
+
         document.querySelector("#numberOfdone").innerHTML = completedCounter;
         myList.innerHTML = '';
         localStorage.clear();
     } 
-    else if(b.target.classList.contains('delete')){ 
-        b.target.parentElement.remove();
-        document.querySelector("#numberOfdone").innerHTML = ++completedCounter;
+    else if(b.target.classList.contains('delete')){
 
-        console.log(b.target.parentElement.textContent.slice(0, -10))
         localStorage.removeItem(b.target.parentElement.textContent.slice(0, -10));
+
+        b.target.parentElement.remove();
+
+        document.querySelector("#numberOfdone").innerHTML = ++completedCounter;
+        
     }
     else if(b.target.classList.contains('done')){
-
-        console.log(b.target.parentElement.textContent.slice(0, -10));
 
         b.target.parentElement.style.textDecoration = 'line-through';
         b.target.textContent = 'not!'
         b.target.classList.remove('done');
         b.target.classList.add('notd');
+
         localStorage.setItem(b.target.parentElement.textContent.slice(0, -10), '0');
     }
     else if(b.target.classList.contains('notd')){
-
-        console.log(b.target.parentElement.textContent.slice(0, -10));
 
         b.target.parentElement.style.textDecoration = 'none';
         b.target.textContent = 'Done'
         b.target.classList.add('done');
         b.target.classList.remove('notd');
+
         localStorage.setItem(b.target.parentElement.textContent.slice(0, -10), '1');
     }
-});//Un
-
+});
 Object.keys(localStorage).forEach((key) => {
     const value = localStorage.getItem(key);
     addElementToList(key, value);
