19a1de5196f8457d588516a9bd844bfd7ccc6fe9
bartoszzieba091-cmd <bartoszzieba091@gmail.com>
2026-05-24 18:15:54 +0200

    to_do_list

---
+++ todolist.js
@@ -0,0 +1,74 @@
+console.log("js is alive");
+
+Object.keys(localStorage).forEach((key) => {
+    const value = localStorage.getItem(key);
+    addElementToList(key, value);
+});
+
+const input = document.querySelector('input');
+const myList = document.querySelector('ol');
+let completedCounter = 0;
+
+function addElementToList(v, operation){
+    localStorage.setItem(v, '1');
+    const newObject = document.createElement('li');
+    newObject.textContent = v;
+
+    document.querySelector('ol').appendChild(newObject);
+
+    const newButton = document.createElement('button');
+
+    if (operation == 1){
+    newButton.textContent = 'Done';
+    newButton.classList.add('done'); 
+    }else{
+    newButton.textContent = 'Unone';
+    newButton.classList.add('undone');
+    newObject.style.textDecoration = 'line-through'
+    }
+    const newButtonDel = document.createElement('button');
+    newButtonDel.textContent = 'Delete';
+    newButtonDel.classList.add('delete');
+
+    newObject.appendChild(newButton);
+    newObject.appendChild(newButtonDel);
+    
+    console.log(localStorage.length);
+}
+
+input.addEventListener('keydown', function(e) {
+    if (e.key === 'Enter') {
+        addElementToList(input.value, 1);
+        input.value = '';
+    }
+});
+
+document.body.addEventListener('click', function(b){
+    if (b.target.id === 'add') {
+        addElementToList(input.value, 1);
+        input.value = '';
+    } 
+    else if (b.target.id === 'allDone') {
+        completedCounter += myList.children.length
+        document.querySelector("#numberOfdone").innerHTML = completedCounter;
+        myList.innerHTML = '';
+        localStorage.clear();
+    } 
+    else if(b.target.classList.contains('delete')){
+        b.target.parentElement.remove();
+        document.querySelector("#numberOfdone").innerHTML = ++completedCounter;
+    }
+    else if(b.target.classList.contains('done')){
+        b.target.parentElement.style.textDecoration = 'line-through';
+        b.target.textContent = 'Undone'
+        b.target.classList.remove('done');
+        b.target.classList.add('undone');
+        lo
+    }
+    else if(b.target.classList.contains('undone')){
+        b.target.parentElement.style.textDecoration = 'none';
+        b.target.textContent = 'Done'
+        b.target.classList.add('done');
+        b.target.classList.remove('undone');
+    }
+});
